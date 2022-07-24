6085d49f251b8a96b357b90df6060956bb5646b3
shaohaozhi <tmoonlight@163.com>
2022-07-24 20:49:06 +0800

    mac，为什么会编译frigate.cpp?

--- src/frigate.cpp
+++ src/frigate.cpp
@@ -1,8 +1,8 @@
-#include "project/frigate.h""
+#include "project/frigate.h"
 
 int x = 3;
 
-Frigate::Frigate() : testInt(0), testInt2(2), testInt3(3), testRefInt4(3)
+Frigate::Frigate() : testInt(0), testInt2(2), testInt3(3)//, testRefInt4(3)
 {
  /* int x =5;
   testRefInt4 = x;*/
